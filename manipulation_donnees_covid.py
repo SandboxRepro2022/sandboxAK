e2ee5f6de8e6fe621f664fff5a4a44afbf0cf624
amine2amayes <amine2amayes@gmail.com>
2022-09-20 00:42:25 +0200

    retrait ligne inutile

--- manipulation_donnees_covid.py
+++ manipulation_donnees_covid.py
@@ -1,6 +1,5 @@
 import pandas as pd
 df=pd.read_csv("https://www.data.gouv.fr/fr/datasets/r/dc7663c7-5da9-4765-a98b-ba4bc9de9079",delimiter=";")
-df.groupby
 df=df.groupby("Semaine").sum()
 df2=df.copy()
 df2=df2.reset_index()
